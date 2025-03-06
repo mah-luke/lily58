f298835b87e35af20a1f0cbd62f82599e061e9e8
Lukas Mahler <lukas.mah@outlook.at>
2025-03-06 10:48:32 +0100

    feat: improve gaming layout by adding LSFT and LCTL keys

--- keyboards/splitkb/aurora/lily58/keymaps/mah-luke/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/mah-luke/keymap.c
@@ -3,7 +3,7 @@
 #include "keymap.h"
 #endif
 
-enum layer_name { _BASE, _F1, _F2, _NUM, _BASE_2, _GAMING, _6 };
+enum layer_name { _BASE, _F1, _F2, _NUM, _BASE2, _GAMING, _6 };
 
 #define KC_AE RALT(KC_Q)
 #define KC_SS RALT(KC_S)
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        KC_BSPC, KC_LGUI, MO(_F1), KC_SPC , KC_ENT , MO(_F2), KC_LCTL , KC_LALT
             ),
 
-    [_FN1] = LAYOUT(
+    [_F1] = LAYOUT(
             KC_F11 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                   KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F12 ,
             _______, _______, _______, _______, _______, _______,                   _______, _______, KC_EQL , KC_LBRC, KC_RBRC, KC_PIPE,
             KC_BSLS, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
@@ -38,12 +38,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        KC_BSPC, KC_LGUI, _______, _______, MO_GAM ,MO(_NUM), KC_LCTL, KC_LALT
             ),
 
-    [_FN2] = LAYOUT(
+    [_F2] = LAYOUT(
             _______, _______, _______, KC_EUR , _______, _______,                   _______, _______, _______, _______, _______, _______,
             _______, KC_SS  , _______, KC_UP  , _______, _______,                   KC_YES , KC_UE  , _______, KC_OE  , KC_PREV, _______,
             _______, KC_AE  , KC_LEFT, KC_DOWN, KC_RGHT, KC_FRWD,                   KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
             _______, _______, _______, _______, _______, KC_BKWD, KC_LCTL, _______, KC_NEXT, _______, _______, _______, _______, _______,
-                                       KC_DEL , KC_LGUI,MO(_NUM), MO(_5) , _______, _______, KC_LCTL, KC_LALT
+                                       KC_DEL , KC_LGUI,MO(_NUM), MO(_6) , _______, _______, KC_LCTL, KC_LALT
             ),
 
     [_NUM] = LAYOUT(
@@ -65,12 +65,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_GAMING] = LAYOUT(
             _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
             _______, _______, KC_Q   , KC_W   , KC_E   , KC_R   ,                   _______, _______, _______, _______, _______, _______,
-            _______, _______, KC_A   , KC_S   , KC_D   , KC_F   ,                   _______, _______, _______, _______, _______, _______,
-            KC_LSFT, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , _______, _______, _______, _______, _______, _______, _______, _______,
+            _______, KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   ,                   _______, _______, _______, _______, _______, _______,
+            KC_LCTL, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , _______, _______, _______, _______, _______, _______, _______, _______,
                                        KC_ENT , KC_LALT, KC_LCTL, _______, _______, _______, _______, QK_LLCK
             ),
 
-    [_5] = LAYOUT(
+    [_6] = LAYOUT(
             _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
             _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
             _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
@@ -88,12 +88,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     // clang-format off
-    [_BASE] = {ENCODER_CCW_CW(MS_WHLD, MS_WHLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [1] =     {ENCODER_CCW_CW(MS_DOWN, MS_UP  ), ENCODER_CCW_CW(MS_LEFT, MS_RGHT)},
-    [2] =     {ENCODER_CCW_CW(KC_PGDN, KC_PGUP), ENCODER_CCW_CW(UG_SPDD, UG_SPDU)},
-    [3] =     {ENCODER_CCW_CW(UG_HUED, UG_HUEU), ENCODER_CCW_CW(UG_SATD, UG_SATU)},
-    [4] =     {ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)},
-    [5] =     {ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)},
+    [_BASE]   = {ENCODER_CCW_CW(MS_WHLD, MS_WHLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [_F1]     = {ENCODER_CCW_CW(MS_DOWN, MS_UP  ), ENCODER_CCW_CW(MS_LEFT, MS_RGHT)},
+    [_F2]     = {ENCODER_CCW_CW(KC_PGDN, KC_PGUP), ENCODER_CCW_CW(UG_SPDD, UG_SPDU)},
+    [_NUM]    = {ENCODER_CCW_CW(UG_HUED, UG_HUEU), ENCODER_CCW_CW(UG_SATD, UG_SATU)},
+    [_BASE2]  = {ENCODER_CCW_CW(MS_WHLD, MS_WHLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [_GAMING] = {ENCODER_CCW_CW(MS_WHLD, MS_WHLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [_6]      = {ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)},
     //clang-format on
 };
 #endif
